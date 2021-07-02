3c1d03d8caaa5c8ad61a63ed19ccfd98c562aacf
LuisAngel <luisangel.salvat@gmail.com>
2021-07-01 21:19:32 -0500

    3. BottomNavigation, material design y fragments.

--- app/src/main/java/com/qbo/appkea5materialdesignfragments/MainActivity.kt
+++ app/src/main/java/com/qbo/appkea5materialdesignfragments/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.qbo.appkea5materialdesignfragments
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.qbo.appkea5materialdesignfragments.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnlogin.setOnClickListener {
+            startActivity(Intent(
+                applicationContext,
+                HomeActivity::class.java
+            ))
+        }
     }
 }
