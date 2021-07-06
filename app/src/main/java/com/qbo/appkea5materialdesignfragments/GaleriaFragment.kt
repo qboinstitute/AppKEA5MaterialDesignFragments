e7fd0036165f6094d47e9ce70fe7afe3e892c1be
LuisAngel <luisangel.salvat@gmail.com>
2021-07-06 18:28:47 -0500

    4. Listado de información en el RecyclerView.

--- app/src/main/java/com/qbo/appkea5materialdesignfragments/GaleriaFragment.kt
+++ app/src/main/java/com/qbo/appkea5materialdesignfragments/GaleriaFragment.kt
@@ -5,8 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.qbo.appkea5materialdesignfragments.adapter.AndroidAdapter
 import com.qbo.appkea5materialdesignfragments.databinding.FragmentGaleriaBinding
 import com.qbo.appkea5materialdesignfragments.databinding.FragmentNotificacionesBinding
+import com.qbo.appkea5materialdesignfragments.datos.DatosAndroid
 
 
 class GaleriaFragment : Fragment() {
@@ -20,6 +23,10 @@ class GaleriaFragment : Fragment() {
         _binding = FragmentGaleriaBinding.inflate(inflater,
             container, false)
         binding.tvtitulogaleria.text = "ESTAMOS EN EL FRAGMENT GALERIA"
+        binding.rvandroidfrag.layoutManager =
+            LinearLayoutManager(requireActivity())
+        binding.rvandroidfrag.adapter =
+            AndroidAdapter(DatosAndroid().listaVersionesAndroid())
         return binding.root
     }
 
--- app/src/main/java/com/qbo/appkea5materialdesignfragments/datos/DatosAndroid.kt
+++ app/src/main/java/com/qbo/appkea5materialdesignfragments/datos/DatosAndroid.kt
@@ -11,55 +11,61 @@ class DatosAndroid {
             listAndroid.size + 1,
             R.drawable.image1,
             "Apple Pie",
-            "1"
+            "Android 1.0"
         ))
         listAndroid.add(Android(
             listAndroid.size + 1,
-            R.drawable.image1,
-            "Apple Pie",
-            "1"
+            R.drawable.image2,
+            "Banana Bread",
+            "Android 1.1"
         ))
         listAndroid.add(Android(
             listAndroid.size + 1,
-            R.drawable.image1,
-            "Apple Pie",
-            "1"
+            R.drawable.image3,
+            "Cupcake",
+            "Android 1.5"
         ))
         listAndroid.add(Android(
             listAndroid.size + 1,
-            R.drawable.image1,
-            "Apple Pie",
-            "1"
+            R.drawable.image4,
+            "Donut",
+            "Android 1.6"
         ))
         listAndroid.add(Android(
             listAndroid.size + 1,
-            R.drawable.image1,
-            "Apple Pie",
-            "1"
+            R.drawable.image5,
+            "Eclair",
+            "Android 2.0 - 2.1"
         ))
         listAndroid.add(Android(
             listAndroid.size + 1,
-            R.drawable.image1,
-            "Apple Pie",
-            "1"
+            R.drawable.image6,
+            "Froyo",
+            "Android 2.2"
         ))
         listAndroid.add(Android(
             listAndroid.size + 1,
-            R.drawable.image1,
-            "Apple Pie",
-            "1"
+            R.drawable.image7,
+            "Gingerbread",
+            "Android 2.3"
         ))
         listAndroid.add(Android(
             listAndroid.size + 1,
-            R.drawable.image1,
-            "Apple Pie",
-            "1"
+            R.drawable.image8,
+            "Honeycomb",
+            "Android 3.0"
         ))
         listAndroid.add(Android(
             listAndroid.size + 1,
-            R.drawable.image1,
-            "Apple Pie",
-            "1"
+            R.drawable.image9,
+            "Jelly Bean",
+            "Android 4.1 - 4.3"
+        ))
+        listAndroid.add(Android(
+            listAndroid.size + 1,
+            R.drawable.image10,
+            "Mashmellow",
+            "Android 4.5"
         ))
         return listAndroid
     }
